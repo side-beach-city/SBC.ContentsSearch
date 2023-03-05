31e4354f6b66e2bef8f44104552861a39e16a296
高見知英 <chie@onpu-tamago.net>
2023-03-05 18:03:19 +0900

    ページタイトルにアプリ名が表示されるように変更

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 function doGet(request) {
-  const template = HtmlService.createTemplateFromFile('res/index');
-  template.sbcast = `const entry_casts = ${JSON.stringify(getRSS())};`;
-  template.openmic = `const entry_openmic = ${JSON.stringify(getData())};`;
-  return template.evaluate();
+  return getHTML();
 }
 
-function test(){
+function getHTML(){
   const template = HtmlService.createTemplateFromFile('res/index');
   template.sbcast = `const entry_casts = ${JSON.stringify(getRSS())};`;
   template.openmic = `const entry_openmic = ${JSON.stringify(getData())};`;
-  Logger.log(template.sbcast);
-  Logger.log(template.openmic);
-  Logger.log(template.evaluate().getContent());
+  const html = template.evaluate();
+  const title = html.getContent().match(/<title>(.*?)<\/title>/i)[1];  
+  html.setTitle(title);
+  return html;
+}
+
+function testHTML(){
+  const html = getHTML();
+  Logger.log(html.getTitle());
+  Logger.log(`Test${html.getTitle() ? "OK" : "NG"}`);
 }
