1f9f66698bea6c8d6407eadd9a7b9a3d5a3340b7
高見知英 <chie@onpu-tamago.net>
2023-03-19 16:20:42 +0900

    音声版があったときにスピーカーマークを表示する機能を付与

--- res/index.js
+++ res/index.js
@@ -58,7 +58,8 @@ document.body.onload = (e) => {
     tmpl.querySelector(".text .collapse").id = id;
     tmpl.querySelector(".text .card-body").innerHTML = e.description.replaceAll("\n", "<br>");
     tmpl.querySelector("span.badge").textContent = e.date;
-    tmpl.querySelector("span.hasVoice").style.display = e.isVoiceContents ? "" : "none";
+    tmpl.querySelector("span.hasVoice").style.display = e.hasVoiceContents ? "" : "none";
+    tmpl.querySelector("span.hasVoice > a.voiceURL").href = e.voiceContentsURL;
     e.tags.forEach(t => {
       let li = document.createElement("li");
       li.textContent = t;
