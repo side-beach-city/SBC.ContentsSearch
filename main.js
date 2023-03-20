51a8478e3b45d66a493c422d23394fc7fccf37d0
高見知英 <chie@onpu-tamago.net>
2023-03-20 14:41:44 +0900

    データのキャッシング処理と表示処理を分離する Fixes #12

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ function doGet(request) {
   return getHTML();
 }
 
+function refreshCache(){
+  const cache = CacheService.getScriptCache();
+  [SCCACHE_KEY, OMCACHE_KEY, BLOGCACHE_KEY].forEach((n) => {
+    cache.remove(n);
+  });
+  getBLOGRSS();
+  getRSS();
+  getData();
+}
+
 function getHTML(){
   const template = HtmlService.createTemplateFromFile('res/index');
   template.sbcast = `const entry_casts = ${JSON.stringify(getRSS())};`;
@@ -17,4 +27,26 @@ function testHTML(){
   const html = getHTML();
   Logger.log(html.getTitle());
   Logger.log(`Test${html.getTitle() ? "OK" : "NG"}`);
+}
+
+function testRefreshCache(){
+  const cache = CacheService.getScriptCache();
+  const cs = [SCCACHE_KEY, OMCACHE_KEY, BLOGCACHE_KEY];
+  let cacheCreate = true;
+  let cacheRefresh = true;
+  cs.forEach((n) => {
+    cache.remove(n);
+  });
+  refreshCache();
+  cs.forEach((n) => {
+    cacheCreate = cacheCreate && (cache.get(n) !== null);
+  });
+  refreshCache();
+  cs.forEach((n) => {
+    cacheRefresh = cacheRefresh && (cache.get(n) !== null);
+  });
+  console.log("refreshCache() Finished");
+  console.log(`Cache none -> create ${cacheCreate ? "OK" : "NG"}`);
+  console.log(`Cache has -> refresh ${cacheRefresh ? "OK" : "NG"}`);
+ 
 }
